e9a980ba1cd35cf9e877ebafb3f778628246c9e6
Angelina Podoļako <angelinapodolako203@gmail.com>
2026-08-25 15:32:38 +0200

    Initial commit: 1,2,3 year folders

---
+++ 2023-2024 -- Year 1/Programming 1&2/problem3ab_wg8_s1125886.py
@@ -0,0 +1,16 @@
+nameq= " Hello! \n I am writing about important figures in the history of programming.\n Write your name, please. "
+print (nameq)
+namea = input ()
+print ( "Your name is "+ namea )
+
+ageq= " What is your year of birth? "
+print (ageq)
+agea = input ()
+print ( "You born in "+ agea )
+
+jobq= " What is your contribution to the field of computer science? "
+print (jobq)
+joba = input ()
+print ( "Your name is "+ joba )
+
+print (namea+ " " +agea+" " + joba)
